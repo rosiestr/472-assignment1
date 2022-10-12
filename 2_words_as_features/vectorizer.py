1df7138db15b50b27e30f16e09762c1e60af6f19
patel-krishna <krishna.patel0621@gmail.com>
2022-10-11 21:57:22 -0400

    Working on Part 2.3

--- 2_words_as_features/vectorizer.py
+++ 2_words_as_features/vectorizer.py
@@ -7,9 +7,11 @@ from sklearn.linear_model import Perceptron
 import gzip
 import json
 import collections
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC 
 
 #EXTRACT DATA FROM ZIP FOLDER
-jsonfiledirectory = "/Users/rosiers/Documents/GitHub/472-assignment1/goemotions.json.gz"
+jsonfiledirectory = "C:\\Users\\Krish\\.vscode\\472-assignment1\\goemotions.json.gz"
 
 with gzip.open(jsonfiledirectory, "r") as f:
     data = json.loads(f.read().decode("utf-8"))
@@ -29,25 +31,37 @@ v_fit = vectorizer.fit_transform(strings)
 X_train, X_test, y_train, y_test = train_test_split(v_fit, sentiment, test_size=0.2)
 
 #2.3
-#Base-MNP ~ 2.3.1
+#Base-MNB ~ 2.3.1
 base_mnb = MultinomialNB()
 base_mnb.fit(X_train,y_train)
 print("Base-MNP: ")
 print(base_mnb.score(X_test,y_test))
 
 #Base-DT ~ 2.3.2
-dtc = DecisionTreeClassifier()
-dtc.fit(X_train, y_train)
-print("Base-DT: ")
-print(dtc.score(X_test, y_test))
+# dtc = DecisionTreeClassifier()
+# dtc.fit(X_train, y_train)
+# print("Base-DT: ")
+# print(dtc.score(X_test, y_test))
 
 #Base-MLP ~ 2.3.3
-p = Perceptron()
-p.fit(X_train,y_train)
-print("Base-MLP: ")
-print(p.score(X_test,y_test))
+# p = Perceptron()
+# p.fit(X_train,y_train)
+# print("Base-MLP: ")
+# print(p.score(X_test,y_test))
 
 #Top-MNB ~ 2.3.4
+tmnb = MultinomialNB()
+params = {
+    "alpha" : [0.5, 0.0, 10.0, 100.0]
+}
+grid_search = GridSearchCV(
+    estimator= tmnb,
+    param_grid= params
+)
+
+grid_search = grid_search.fit(X_train, y_train)
+print("TOP_MNB:")
+print(grid_search.score(X_test, y_test))
 
 
 #Top-DT ~ 2.3.5
